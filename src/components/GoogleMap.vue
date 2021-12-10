4f88b07201bb0cf07cfcdf100d3a62e4282ea0dc
MadhushaPrasad <madushaprasad21@gmail.com>
2021-12-10 11:36:36 +0530

    feat(google map):create mark function

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,10 +1,79 @@
 <template>
-  <div>google map</div>
+  <div>
+    <button @click="drowMarkers">drow markers</button>
+    <button @click="drowDerections">drow derection</button>
+    <button @click="clearMap">clear map</button>
+    <GmapMap
+      :center="{ lat: 10, lng: 10 }"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 1000px; height: 300px"
+      @click="markLocation($event)"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      />
+    </GmapMap>
+  </div>
 </template>
 
 <script>
+const home = {
+  lat: 53.462118,
+  lng: -2.249097,
+};
+const work = {
+  lat: 53.464144,
+  lng: -2.249828,
+};
+
 export default {
   name: 'googleMap',
+  data() {
+    return {
+      markers: [],
+    };
+  },
+
+  methods: {
+    //show mark what user click
+    markLocation(event) {
+      this.markers = [
+        {
+          position: {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng(),
+          },
+        },
+      ];
+      console.log(event.latLng.lat());
+      console.log(event.latLng.lng());
+    },
+
+    drowMarkers() {
+      this.markers = [
+        {
+          position: home,
+          label: 'Home',
+        },
+        {
+          position: work,
+          label: 'Work',
+        },
+      ];
+    },
+    drowDerections() {
+      alert('drowDerections');
+    },
+    clearMap() {
+      alert('clearMap');
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,14 +6,7 @@ Vue.config.productionTip = false;
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'YOUR_API_TOKEN',
-    libraries: 'places', // This is required if you use the Autocomplete plugin
-    // OR: libraries: 'places,drawing'
-    // OR: libraries: 'places,drawing,visualization'
-    // (as you require)
-
-    //// If you want to set the version, you can do so:
-    // v: '3.26',
+    key: 'AIzaSyA8YNqOcYThvHB4ePTi49xAAkDxsV88JpU',
   },
 });
 
