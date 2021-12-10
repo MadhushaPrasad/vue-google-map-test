3991b81e19fadff9fdc66e40a1a0a28a8999b057
MadhushaPrasad <madushaprasad21@gmail.com>
2021-12-10 10:48:12 +0530

    feat(google map):create google map

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GoogleMap />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoogleMap from './components/GoogleMap.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    GoogleMap,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>google map</div>
+</template>
+
+<script>
+export default {
+  name: 'googleMap',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
